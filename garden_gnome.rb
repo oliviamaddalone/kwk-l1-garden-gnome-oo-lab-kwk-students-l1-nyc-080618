6c91e74e088b0fe6c55414330fb1f28ba25bf29b
oliviamaddalone <livmaddalone@gmail.com>
2018-08-10 17:32:29 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,30 @@
 # Code your instances here
+class GardenGnome
+ 
+  def initialize
+   @name=name
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def age
+    @age=age
+  end
+  
+  def age 
+    @age 
+  end 
+  
+  def gluten_allergy
+    @gluten_allergy=gluten_allergy
+  end 
+  
+  def gluten_allergy
+    @gluten_allergy
+  end 
+  
+  
+ 
+ end  
